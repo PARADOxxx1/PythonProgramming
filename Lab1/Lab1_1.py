84d28ff74adc4e65250cf7a56b8336a49cf3416a
PARADOxxx1 <65506506+PARADOxxx1@users.noreply.github.com>
2023-10-17 11:54:27 +0300

    Update Lab1_1.py

--- Lab1/Lab1_1.py
+++ Lab1/Lab1_1.py
@@ -3,5 +3,5 @@ numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -
 # TODO заменить значение пропущенного элемента средним арифметическим
 
 ind_none = numbers.index(None)
-numbers[ind_none] = sum(numbers[:ind_none] + numbers[ind_none + 1:]) / len(numbers)
+numbers[ind_none] = (sum(numbers[:ind_none]) + sum(numbers[ind_none + 1:])) / len(numbers)
 print("Измененный список:", numbers)
