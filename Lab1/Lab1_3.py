e33489433ff8c361fb04aedf8e1ba56264b0ade5
PARADOxxx1 <65506506+PARADOxxx1@users.noreply.github.com>
2023-10-17 12:10:55 +0300

    Update Lab1_3.py

--- Lab1/Lab1_3.py
+++ Lab1/Lab1_3.py
@@ -1,6 +1,7 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
 
 # TODO Разделите участников на две команды
-team1 = list_players[:len(list_players) // 2]
-team2 = list_players[len(list_players) // 2:]
+ind_for_teams = len(list_players) // 2
+team1 = list_players[:ind_for_teams]
+team2 = list_players[ind_for_teams:]
 print(team1, team2, sep = '\n')
